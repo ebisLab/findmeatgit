c9f29c6efeccd0c206636442eaaea28c196f4bbe
ebisLab <ebaiden@techie.com>
2020-12-07 19:54:16 -0500

    user can be searched

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'
 
 function App() {
+  const [users, setUsers]=useState([])
+  const [followers, setFollowers]=useState([])
+  const [userText, setUserText]=useState('')
+
+  // useEffect(() => {
+  //   axios
+  //   .get(`https://api.github.com/users/${userText}`)
+  //   .then(res=>setUsers(res.data))
+  // }, [])
+
+  const fetchUsers =e=>{
+    e.preventDefault()
+    axios
+    .get(`https://api.github.com/users/${userText}`)
+    .then(res=>{
+      setUsers(res.data)
+      console.log("res", res)}
+    )
+    .catch(err=>console.log(err))
+  }
+
+  const handleChange=e=>{
+    setUserText(e.target.value)
+  }
+
+  console.log("SET USERS", users)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={fetchUsers}>
+        <input
+        placeholder="Search User..."
+        type="text"
+        value={userText}
+        onChange={handleChange}
+         />
+         <button>Search</button>
+      </form>
+
+      <div>
+       {users.login}
+       <img src={users.avatar_url} style={{height:"100px"}}/>
+
+      </div>
     </div>
   );
 }
